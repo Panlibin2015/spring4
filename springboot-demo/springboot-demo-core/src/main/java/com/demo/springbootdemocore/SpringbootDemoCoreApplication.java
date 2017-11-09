68dd8e2c8dcdd91102c2b8e01b41e1ef09f8b58c
Panlibin2015 <18267568873@163.com>
2017-11-09 17:25:05 +0800

    new project springboot-demo

---
+++ springboot-demo/springboot-demo-core/src/main/java/com/demo/springbootdemocore/SpringbootDemoCoreApplication.java
@@ -0,0 +1,12 @@
+package com.demo.springbootdemocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoCoreApplication.class, args);
+	}
+}
